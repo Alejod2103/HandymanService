c1c5642cc6b2dc9e60b372623a770e95b1fb5e90
alejodev2103 <alejod2103@gmail.com>
2022-07-19 17:18:54 -0600

    añadiendo nueva seccion

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import '../../css/style.css';
 import About from '../about/About.js'
 import { Carousel } from '../carousel/Carousel.js'
 import { Info } from '../info/Info.js'
+import Servicios from '../servicios/Servicios.js'
 
 function Home() {
   return (
     <>
       <Carousel />
-      
       <Info />
-
       <About />
+      <Servicios />
     </>
   )
 }
